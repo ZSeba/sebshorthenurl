3341720dbc85f33d4da4e9460b84daf64f7d562c
Sebastian Vidal <sebastian.vidalm@gmail.com>
2018-03-29 04:06:22 -0700

    Url redirection
    I'm using the show controller action to redirect to the original url, also using anything after the "/" in the address to assume it's the shortened url, so "localhost:3000/abc" redirects to "www.noomii.com" as in the example in the instructions.

--- shorturl/app/controllers/urls_controller.rb
+++ shorturl/app/controllers/urls_controller.rb
@@ -5,4 +5,10 @@ class UrlsController < ApplicationController
 
     def create
     end
+
+    def show
+        @url = Url.find_by_shortened_url(params[:random_string])
+        puts params.inspect
+        redirect_to @url.original_url
+    end
 end
--- shorturl/test/controllers/urls_controller_test.rb
+++ shorturl/test/controllers/urls_controller_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class UrlsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should redirect to the original url" do
+    # test_url is a URL with this data: {original_url: "ww.batman.com", short_url: batman}
+    # This tests if localhost:3000/batman redirects to "www.batman.com"
+    test_url = urls(:one)
+    get "/#{test_url.shortened_url}"
+    assert_redirected_to test_url.original_url
+  end
 end
