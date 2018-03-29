bbd89a9a7ca519b5ca465b096498d4b670644b79
Sebastian Vidal <sebastian.vidalm@gmail.com>
2018-03-29 03:23:42 -0700

    basic validation test
    Test if an URL is saved without specifying first an original URL

--- shorturl/test/models/url_test.rb
+++ shorturl/test/models/url_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class UrlTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "should not save url without original_url" do
+    url = Url.new
+    assert_not url.save
+  end
 end
